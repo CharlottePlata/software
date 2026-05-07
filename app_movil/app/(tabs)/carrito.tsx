69c3840336b08ea54f48e54b637048d1cce14028
CharlottePlata <BCPR31912393@soy.sena.edu.co>
2026-05-07 15:35:57 -0500

    .

---
+++ app_movil/app/(tabs)/carrito.tsx
@@ -0,0 +1,6 @@
+/**
+ * Pnatall de carrito de compras y sus respectivas gestiones no requiere que se autenticado solo para hacer compras
+ */
+
+//importar componetes de react native para construir la pantalla
+import { ActivityIndicator, Alert, Image, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
